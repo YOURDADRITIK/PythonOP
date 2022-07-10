3f6b473a946e2f04393fa00ded32d4b9e93caa8e
MK <101259067+ERR0rMK@users.noreply.github.com>
2022-07-10 19:06:03 +0530

    Add files via upload

--- Python.py
+++ Python.py
@@ -779,7 +779,9 @@ async def spam(e):
 
 async def spam(e):
     if e.sender_id in MK_USERS:
-        message = e.raw_text
+        aries = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        await e.get_reply_message()
+        message = str(aries[1])
         a = await e.client.get_entity(message)
         g = a.id
         c = a.first_name
